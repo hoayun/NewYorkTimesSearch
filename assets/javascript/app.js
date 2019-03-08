cd899238e369efb9974f4a1bb1cf4251ade9027c
Chris Hoayun <45009760+hoayun@users.noreply.github.com>
2019-03-08 14:02:53 -0500

    css changes linked javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,7 @@ $.ajax ({
 
 
 ////URL query if dates entered
-queryURLSearchDate = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=" + input + "&facet_field=day_of_week&facet=true&begin_date=" + startYear + "0101&end_date=" + endYear + "0101&api-key=t4lC5ztOwGuKqRDv8AziAiygSX0OiEtf"
+/*queryURLSearchDate = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=" + input + "&facet_field=day_of_week&facet=true&begin_date=" + startYear + "0101&end_date=" + endYear + "0101&api-key=t4lC5ztOwGuKqRDv8AziAiygSX0OiEtf"
 
 $.ajax ({
     URL: queryURLSearchDate,
