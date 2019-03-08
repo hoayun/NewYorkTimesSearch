5f395088c28b09438e027d25054b5a3a574146fc
elcid00 <elcid00@aol.com>
2019-03-08 13:49:35 -0500

    java added

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+
+
+////URL query for just key word search
+queryURLSearch = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + input + "&api-key=t4lC5ztOwGuKqRDv8AziAiygSX0OiEtf"
+
+$.ajax ({
+    URL: queryURLSearch,
+    Method: "GET"
+}).then(function(response){
+    console.log(response);
+    console.log(response.runtime)
+});
+
+
+
+////URL query if dates entered
+queryURLSearchDate = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=" + input + "&facet_field=day_of_week&facet=true&begin_date=" + startYear + "0101&end_date=" + endYear + "0101&api-key=t4lC5ztOwGuKqRDv8AziAiygSX0OiEtf"
+
+$.ajax ({
+    URL: queryURLSearchDate,
+    Method: "GET"
+}).then(function(response){
+    console.log(response);
+    console.log(response.runtime)
+});
